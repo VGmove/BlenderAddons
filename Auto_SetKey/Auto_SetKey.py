7302767aa5a0167cbaece8616fa472a6d482d908
VGmove <148340867+VGmove@users.noreply.github.com>
2024-07-31 17:02:25 +0300

    Update Auto_SetKey.py

--- Auto_SetKey/Auto_SetKey.py
+++ Auto_SetKey/Auto_SetKey.py
@@ -31,7 +31,7 @@ from bpy.props import (StringProperty,
                        FloatProperty,
                        EnumProperty,
                        PointerProperty,
-					   FloatVectorProperty,
+		       FloatVectorProperty,
                        )
 from bpy.types import (Panel,
                        Operator,
